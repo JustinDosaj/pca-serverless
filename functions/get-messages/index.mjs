c673c5627cb801cfbdbf5cf75bcb357400284220
JustinDosaj <justindosaj@gmail.com>
2025-04-07 15:11:17 -0700

    Update Get Messages

--- functions/get-messages/index.mjs
+++ functions/get-messages/index.mjs
@@ -1,5 +1,26 @@
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { DynamoDBDocumentClient, QueryCommand } from "@aws-sdk/lib-dynamodb";
+
+// TODO: Get conversation id from event then get all messages from dynamodb
+const dynamo = DynamoDBDocumentClient.from(new DynamoDBClient({ region: "us-west-1" }));
+const MESSAGES_TABLE_NAME = 'dev_Messages';
+
 export const handler = async (event) => {
     try {
+        
+        const userId = event.requestContext.authorizer.jwt.claims.sub
+        const body = JSON.parse(event.body)
+        const conversationId = body.conversationId || ''
+
+        const result = await dynamo.send(new QueryCommand({
+            TableName: MESSAGES_TABLE_NAME,
+            KeyConditionExpression: "conversationId = :cid",
+            FilterExpression: "userId = :uid",
+            ExpressionAttributeValues: {
+                ":cid": conversationId,
+                ":uid": userId,
+            }
+        }))
 
         return {
             statusCode: 200,
@@ -8,7 +29,7 @@ export const handler = async (event) => {
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify({
-                content: 'Get Messages Successfully Setup'
+                messages: result.Items || [],
             })
             };
     } catch (error) {
